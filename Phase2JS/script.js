9f1b80cd284089d4d19df20acc7102c1f381709c
Ankit Kumar Nayak <ankitkumarnayak006@gmail.com>
2026-05-28 23:35:02 +0530

    Questions

---
+++ Phase2JS/script.js
@@ -0,0 +1,55 @@
+console.log("Hii Ankit");
+
+// function greet(){
+//     console.log("Hello World");
+// }
+// greet()
+
+// function add(a,b){
+//     return a+b
+// }
+// console.log(add(12,34))
+
+// function square(a){
+//     return a*a;
+// }
+// console.log(square(2));
+
+// function check(a) {
+//   if (a % 2 == 0) {
+//     return "Even";
+//   } else {
+//     return "odd";
+//   }
+// }
+// console.log(check(3))
+
+// function temp(c) {
+//   let F = c * (9 / 5) + 32;
+//   console.log(`${F} F`);
+// }
+// console.log(temp(25));
+
+// function Guest(name="Sir"){
+//     console.log(`Good Night ${name}`);
+// }
+// Guest()
+
+// function greatest(a,b){
+//     var ans = a>b ? a:b;
+//     return ans;
+// }
+// console.log(greatest(6,7));
+
+// function area(l,b){
+//     return l*b;
+// }
+// console.log(area(6,7))
+
+function vote(age) {
+  var vot = age > 18 ? "Adult" : "Minor";
+
+  return vot;
+}
+
+console.log(vote(62));
